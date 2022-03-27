91b9f12be173fca578b5ef05253da3d8cfde1619
Debdeep Pal <deep64222@gmail.com>
2022-03-27 21:05:13 +0530

    added content

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 function scrollToTop() {
   window.scrollTo(0, 0);
 }
+window.addEventListener('scroll', () => {
+  let nav = document.getElementsByClassName("nav")[0]
+  // console.log(window.scrollY)
+  if(window.scrollY>300)
+    nav.style.background="black"
+  else
+    nav.style.background="none"
+
+})
